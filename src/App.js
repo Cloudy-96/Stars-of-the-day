a4b39cb9d5a3e794ebfd752e303b13ad72ce3a32
unknown <katdemey@outlook.com>
2023-06-08 17:10:27 +0100

    initial setup complete. TODO connect .env API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { API_KEY } from "./config.js";
+import "./App.css";
+
+const initialImage = {
+  copyright: "loading...",
+  date: "loading...",
+  explanation: "loading...",
+  hdurl: "loading...",
+  media_type: "loading...",
+  service_version: "loading...",
+  title: "loading...",
+  url: "https://api.nasa.gov/assets/img/favicons/favicon-192.png",
+};
 
 function App() {
+  const [dailyImage, setDailyImage] = useState(initialImage);
+ const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => setDailyImage(data))
+    .then(() => setLoading(false));
+    // .then(console.log("dailyImage:::::::", dailyImage.url));
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Space of the day</h1>
       </header>
+
+      <main>
+        <img src={dailyImage.url} alt="stars of the day" />
+        <h2>Title: {dailyImage.title}</h2>
+        <p>copyright: {dailyImage.copyright}</p>
+        <p>date: {dailyImage.date}</p>
+        <p>{dailyImage.explanation} </p>
+      </main>
     </div>
   );
 }
