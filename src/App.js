e975c33dcd1bbf861feace04573f7c2d9f9e251d
unknown <katdemey@outlook.com>
2023-06-29 00:03:20 +0100

    loading page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import dayjs from "dayjs";
 import { API_KEY } from "./config.js";
 import "./App.css";
+import { PongSpinner } from "react-spinners-kit";
 
 function App() {
   const [dailyImage, setDailyImage] = useState([]);
@@ -17,41 +18,53 @@ function App() {
       .then(() => setLoading(false));
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+  // if (loading) {
+  //   return (
+  //     <div className="Loading">
+  //       <PongSpinner color="#fafafa" />
+  //       <p>Loading...</p>
+  //     </div>
+  //   );
+  // } else {
   return (
     <div className="App">
       <header className="App-header">
         <h1>NASA's Astronomy Picture of the Day</h1>
       </header>
 
-      <main>
-        <img src={dailyImage.url} alt="stars of the day" />
+      {/* conditional loading on main page */}
+      {loading ? (
+        <div className="Loading">
+          <PongSpinner color="#fafafa" />
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <main>
+          <img src={dailyImage.url} alt="stars of the day" />
 
-        <section>
-          <div className="content">
-            <h2>Title: {dailyImage.title}</h2>
-            <h3>Image credit & copyright: {dailyImage.copyright}</h3>
-            <h4>
-              Date: {formattedDate}
-              {/* {dailyImage.date} */}
-            </h4>
-            <br />
-            <p>{dailyImage.explanation} </p>
-          </div>
+          <section>
+            <div className="content">
+              <h2>Title: {dailyImage.title}</h2>
+              <h3>Image credit & copyright: {dailyImage.copyright}</h3>
+              <h4>
+                Date: {formattedDate}
+                {/* {dailyImage.date} */}
+              </h4>
+              <br />
+              <p>{dailyImage.explanation} </p>
+            </div>
 
-          <div>
-            {/* TODO: see how single digit day displays and change it to Mom's Bday */}
-            {dayMonth === "28 Jun" ? (
-              <p> Happy Birthday, Mom!!!</p>
-            ) : (
-              console.log("surprise tbd")
-            )}
-          </div>
-        </section>
-      </main>
+            <div>
+              {/* TODO: see how single digit day displays and change it to Mom's Bday */}
+              {dayMonth === "28 Jun" ? (
+                <p> Happy Birthday, Mom!!!</p>
+              ) : (
+                console.log("surprise tbd")
+              )}
+            </div>
+          </section>
+        </main>
+      )}
 
       <footer>
         <p>redesigned by K.C.D 2023</p>
@@ -70,6 +83,7 @@ function App() {
       </footer>
     </div>
   );
+  // }
 }
 
 export default App;
