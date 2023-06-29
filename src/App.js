4b8dea69ad6efaa683a7e6a1a8e9046a4a9ee1b8
unknown <katdemey@outlook.com>
2023-06-29 21:08:20 +0100

    confetti for birthday surprise implemented. TODO: debug setting timeout so confetti switches off after a few seconds

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import dayjs from "dayjs";
 import { API_KEY } from "./config.js";
 import "./App.css";
 import { PongSpinner } from "react-spinners-kit";
+import { useWindowSize } from "@react-hook/window-size";
+import Confetti from "react-confetti";
 
 function App() {
   const [dailyImage, setDailyImage] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isFalling, setIsFalling] = useState(false);
+  const [width, height] = useWindowSize();
   const date = dailyImage.date;
   const formattedDate = dayjs(date).format("DD MMM YYYY");
   const dayMonth = dayjs(date).format("DD MMM");
@@ -18,14 +22,13 @@ function App() {
       .then(() => setLoading(false));
   }, []);
 
-  // if (loading) {
-  //   return (
-  //     <div className="Loading">
-  //       <PongSpinner color="#fafafa" />
-  //       <p>Loading...</p>
-  //     </div>
-  //   );
-  // } else {
+  /* TODO: set timer for how long confetti will load for: use isFalling*/
+  if (dayMonth === "02 Jul" || dayMonth === "29 Jun") {
+    setTimeout(() => {
+      setIsFalling(true);
+    }, 3000);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -40,50 +43,55 @@ function App() {
         </div>
       ) : (
         <main>
-          <img src={dailyImage.url} alt="stars of the day" />
+          <img
+            className="dailyImage"
+            src={dailyImage.url}
+            alt="stars of the day"
+          />
 
           <section>
-            <div className="content">
-              <h2>Title: {dailyImage.title}</h2>
-              <h3>Image credit & copyright: {dailyImage.copyright}</h3>
-              <h4>
-                Date: {formattedDate}
-                {/* {dailyImage.date} */}
-              </h4>
-              <br />
-              <p>{dailyImage.explanation} </p>
-            </div>
-
             <div>
-              {/* TODO: see how single digit day displays and change it to Mom's Bday */}
-              {dayMonth === "28 Jun" ? (
-                <p> Happy Birthday, Mom!!!</p>
+              {dayMonth === "29 Jun" || dayMonth === "02 Jul" ? (
+                <div className="mom">
+                  {" "}
+                  {isFalling ? (
+                    <div>
+                      <Confetti width={width} height={height} />{" "}
+                      <p>Happy Birthday, Mom!!!</p>
+                    </div>
+                  ) : (
+                    <p>Happy Birthday, Mom!!!</p>
+                  )}
+                </div>
               ) : (
                 console.log("surprise tbd")
               )}
+            </div>
+
+            <div className="content">
+              <h2>Title: {dailyImage.title}</h2>
+              <h3>Image credit & copyright: {dailyImage.copyright}</h3>
+              <h4>Date: {formattedDate}</h4>
+              <br />
+              <p>{dailyImage.explanation} </p>
             </div>
           </section>
         </main>
       )}
 
       <footer>
-        <p>redesigned by K.C.D 2023</p>
+        <p>Redesigned by K.C.D 2023</p>
         <hr />
         <hr />
         <p>
           See original at{" "}
-          <a
-            href="https://apod.nasa.gov/apod/astropix.html"
-            target="blank"
-            nonref
-          >
+          <a href="https://apod.nasa.gov/apod/astropix.html" target="blank">
             NASA
           </a>
         </p>
       </footer>
     </div>
   );
-  // }
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // require("dotenv").config();
 // import env from "react-dotenv";
 
-export const API_KEY =process.env.REACT_APP_NASA_API_KEY
+export const API_KEY = process.env.REACT_APP_NASA_API_KEY
